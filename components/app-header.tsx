c959b44a1f3be7e8b14921142f8c32f8ee76fccf
Siyuan You <satellitem@me.com>
2022-01-02 14:58:01 +1100

    about dialog

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -1,6 +1,6 @@
-import { FolderOpen } from "@mui/icons-material"
-import { AppBar, Button, Stack, Toolbar, Typography } from "@mui/material"
-import { useContext } from "react"
+import { FolderOpen, Info } from "@mui/icons-material"
+import { AppBar, Button, Dialog, DialogContent, DialogContentText, DialogTitle, Stack, Toolbar, Typography } from "@mui/material"
+import { useContext, useState } from "react"
 import useAppState from "../lib/hooks/use-app-state"
 import useWebAPI from "../lib/hooks/use-web-api"
 import AppContext from "../state/app-context"
@@ -9,6 +9,9 @@ const AppHeader = () => {
   const { state, dispatch } = useContext(AppContext)
   const { hasFileSystemAccess } = useWebAPI()
   const { hasFolderSelected } = useAppState()
+
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false)
+
   const handlePickFolder = () => {
     window.showDirectoryPicker()
       .then(dir => dispatch && dispatch({
@@ -34,8 +37,19 @@ const AppHeader = () => {
           hasFileSystemAccess
           && <Button onClick={handlePickFolder} startIcon={<FolderOpen />} color="inherit">Open Folder</Button>
         }
+        <Button onClick={() => setIsDialogOpen(true)} startIcon={<Info />} color="inherit">About</Button>
       </Toolbar>
     </AppBar>
+    <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+      <DialogTitle>
+        About this website
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          This website reads the content of a folder that is picked by you using <a href="https://web.dev/file-system-access/" target="_blank" rel="noreferrer">File System Access API</a> and displays images within that folder. Images are upscaled by <a href="https://github.com/thekevinscott/UpscalerJS" target="_blank" rel="noreferrer">UpscalerJS</a>.
+        </DialogContentText>
+      </DialogContent>
+    </Dialog>
   </>
 }
 export default AppHeader
